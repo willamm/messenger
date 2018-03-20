8de72e0375a0de79ef147dca977bb7d1d09e329a
Will Murphy <will.murphy1994@gmail.com>
2018-03-20 13:59:08 -0700

    Initial commit

---
+++ src/client/main.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018-03-20 01:06:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+
+
+int main(int argc, char** argv)
+{
+    int listenFd;
+
+    printf("Hello");
+    return 0;
+}
---
+++ src/server/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018-03-20 01:14:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("hello from server");
+    return 0;
+}
